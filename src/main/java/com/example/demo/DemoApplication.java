a4adc0516b3fa1e0204042b4efacde3cda81aa2e
root <root@cicd-01.europe-west2-c.c.cicd-334513.internal>
2021-12-09 12:53:52 +0000

    Changed app text output

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,7 +16,7 @@
                   }
                   
                   @GetMapping("/hello")
-                  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+                  public String hello(@RequestParam(value = "name", defaultValue = "Branching test") String name) {
                   return String.format("Hello %s!", name);
                   }
                 
